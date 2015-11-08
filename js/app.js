fbf61711051eb8db728877c6bd42432a717f759e
Alexander <allimon315@gmail.com>
2015-11-08 23:37:37 +0000

    did sum gud stuff

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var app = angular.module("football", ["ngRoute"]);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'templates/index.html',
+        controller: 'IndexController'
+      });
+  }
+]);
