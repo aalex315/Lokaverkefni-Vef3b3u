7e577e55fcd9f5e523c08fcb9132983f30c3c449
Alexander <allimon315@gmail.com>
2015-11-09 00:42:29 +0000

    now gets data from api

--- js/controllers/IndexController.js
+++ js/controllers/IndexController.js
@@ -1,3 +1,11 @@
-app.controller('IndexController', function($scope){
-	$scope.soccer = 
+app.controller('IndexController', function($scope, $http){
+	$http({
+		method: 'GET',
+		url: 'http://apis.is/sports/football/male-leagues/pepsi'
+	}).then(function successCallback(responce){
+		$scope.soccer = responce.data;
+		console.log(responce);
+	}, function errorCallback(responce){
+		console.log(responce);
+	});
 });
