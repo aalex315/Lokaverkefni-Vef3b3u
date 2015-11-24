90920ce643b9dcbec4c5801765b2dfb37644dc98
Alexander <allimon315@gmail.com>
2015-11-24 09:24:54 +0000

    changes some stuff

--- js/controllers/IndexController.js
+++ js/controllers/IndexController.js
@@ -4,7 +4,6 @@ app.controller('IndexController', function($scope, $http){
 		url: 'http://apis.is/sports/football/male-leagues/pepsi'
 	}).then(function successCallback(responce){
 		$scope.matches = responce.data.results;
-		console.log(responce);
 	}, function errorCallback(responce){
 		console.log(responce);
 	});
